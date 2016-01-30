1d3327e82a258de7c24f7518b17a1434e098a0f4
KimTaeIk <tw2614@gmail.com>
2016-01-31 00:45:24 +0900

    UI2

--- src/OxygenSaturationPanel.java
+++ src/OxygenSaturationPanel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Window;
@@ -12,11 +13,13 @@ public class OxygenSaturationPanel extends JPanel{
 	
 	private JFrame _mainFrame;
 	private CircleGraph _oxygenSaturationCircleGraph;
+	private CircleGraph _heartRateCircleGraph;
 	private PulseGraph _pulseGraph;
 	private static final Random random = new Random();
 	public OxygenSaturationPanel() {
 		_oxygenSaturationCircleGraph = new CircleGraph();
 		_pulseGraph = new PulseGraph();
+		_heartRateCircleGraph = new CircleGraph();
 		setUp_GUI();
 	}
 	
@@ -30,9 +33,11 @@ public class OxygenSaturationPanel extends JPanel{
 		this.setBounds(0,0,Main._FRAME_WIDTH,Main._FRAME_HEIGHT);
 		this.setLayout(null);
 		//산소포화도 그래프 삽입.
-		_oxygenSaturationCircleGraph.setBounds( 335,600,
-			CircleGraph._circleGraphWidth,CircleGraph._circleGraphHeight);
+		_oxygenSaturationCircleGraph.setBounds( 510,580,CircleGraph._circleGraphWidth,CircleGraph._circleGraphHeight);
 		this.add(_oxygenSaturationCircleGraph);
+		//심박수 원형 그래프 삽입.
+		_heartRateCircleGraph.setBounds(515,70,CircleGraph._circleGraphWidth,CircleGraph._circleGraphHeight);
+		this.add(_heartRateCircleGraph);
 		//맥박 그래프 삽입.
 		_pulseGraph.setBounds(90,275,PulseGraph.GRAPH_WIDTH,PulseGraph.GRAPH_HEIGHT);
 		this.add(_pulseGraph);
@@ -43,7 +48,23 @@ public class OxygenSaturationPanel extends JPanel{
 	@Override
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2D = (Graphics2D) g;
+		g2D.setColor(Color.WHITE);
+		g2D.fillRect(0,0,Main._FRAME_WIDTH,Main._FRAME_HEIGHT);
 		g2D.setColor(Color.ORANGE);
 		g2D.fillOval(0, 0,Main._FRAME_WIDTH,Main._FRAME_HEIGHT);
+		
+		
+		g2D.setFont(new Font("Arial",Font.BOLD ,80));
+		g2D.setColor(Color.WHITE);
+		g2D.drawString(" Heartrate", 130,160);
+		g2D.setFont(new Font("Arial",Font.BOLD ,60));
+		g2D.drawString("    per Minute", 110,220);
+		
+		g2D.setFont(new Font("Arial",Font.BOLD ,80));
+		g2D.setColor(Color.WHITE);
+		g2D.drawString("   Oxygen", 100,670);
+		g2D.setFont(new Font("Arial",Font.BOLD ,70));
+		g2D.drawString("  Saturation", 100,730);
+	
 	}
 }
